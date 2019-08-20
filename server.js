c2353cf99efc51524d1cb765a49ada61dcdcfd44
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-08-20 18:16:35 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
-//let us load the HTTP module by typing in constant HTTP name and, we can set the constant HTTP name to a require HTTP value for example:
+// Let us load the HTTP module by typing in constant HTTP name and, we can set the constant HTTP name to a require HTTP value for example:
 const http = require('http');
 
+// We can define the function in this server.js module for creating  a web server. So, let us store our value results in a server object by creating a constant name.
+const server = http.createServer();
