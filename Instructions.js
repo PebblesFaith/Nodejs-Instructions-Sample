32913340731bbf87e7cac11d07edab97fbc5f874
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-08-20 15:36:27 -0500

    Create Instructions.js

---
+++ Instructions.js
@@ -0,0 +1,6 @@
+//console.log("Hello World! Our World is not as big as the Universe.  We are a small dust compare to our Universe.");
+//console.log(module);
+const log =  require('./logger');
+//console.log(logger);
+log('message');
+
