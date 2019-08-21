9e759f31d59feb52e641d3b7cda60dd5e90f3131
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-08-20 19:35:48 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,3 +3,8 @@ const http = require('http');
 
 // We can define the function in this server.js module for creating  a web server. So, let us store our value results in a server object by creating a constant name.
 const server = http.createServer();
+
+// Type in server dot (.) listen; in order to call the server then we will need to give the listen on an object name let us say, 3000 for local host port 3000. 
+server.listen(3000);
+
+
