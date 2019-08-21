9368ece5e96d6852e8ea22d1821a8e6cd2c6921b
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-08-21 14:44:14 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,16 @@ const http = require('http');
 // We can define the function in this server.js module for creating  a web server. So, let us store our value results in a server object by creating a constant name.
 const server = http.createServer();
 
+// So, we can use the ‘ON’ method, in order to handle this event emitter, and before listening to the port 3000, we want to register our listener or handler by typing in, server dot (.) on and the name of the event emitter is ‘CONNECTION,’ as defined in the Net Module documentations
+// This listener is a function with (1) argument in which, a socket is a type of socket class and the socket class returns a void; therefore, we add the arrow function (=>) syntax in ‘ES6.’ 
+// ’ Let us add an arrow function for which, means, we are going to use the arrow function after, ‘SOCKET,’ as a first (1st) function that will take a socket to our code block.
+server.on('connection', (socket) => {
+    console.log('New connection… Hello World! I am learning how to use Node.js’ net.Socket listener.');
+
+});
+
 // Type in server dot (.) listen; in order to call the server then we will need to give the listen on an object name let us say, 3000 for local host port 3000. 
 server.listen(3000);
 
-// Create an output message onto the server.
+// Create an output liston on message onto the server.
 console.log('Listening on port 3000…Hello World! I am learning Node.js.');
